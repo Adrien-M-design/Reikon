2ebb0d172de701ea67217c07de6be2c67d83c589
YokoNomura <64535120+YokoNomura@users.noreply.github.com>
2022-04-07 15:51:52 +0200

    UI_talisman_Wip_update

--- Assets/Scripts/UI/TalismanController.cs
+++ Assets/Scripts/UI/TalismanController.cs
@@ -8,16 +8,28 @@ public class TalismanController : MonoBehaviour
 {
     [SerializeField] private GameObject _talismanA = null;
     [SerializeField] private GameObject _talismanB = null;
+    [SerializeField] private TextMeshProUGUI _effectDesc = null;
+    [SerializeField] private Image _tWeightImage = null;
+
+    [SerializeField] private bool _tPickedUp = false;
+
+    private bool _isAEquipped = false;
+    private bool _isBEquipped = false; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _talismanB.SetActive(false);        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_tPickedUp == true)
+        {
+            _talismanB.SetActive(true);
+        }
+
+
     }
 }
