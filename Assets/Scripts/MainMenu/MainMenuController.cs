23a604e5f7ee86e08cc6bafe2c33efca93a1d5c3
YokoNomura <64535120+YokoNomura@users.noreply.github.com>
2022-06-06 15:12:07 +0200

    Screen de credits Main Menu (Mais anim marche pas)

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -5,9 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject _creditsScreen = null;
+    [SerializeField] private GameObject _peopleList = null;
+
+    void Start()
+    {
+        _creditsScreen.SetActive(false);
+        
+    }
+
     public void Play()
     {
         GameStateManager.Instance.LaunchTransition(EGameState.GAME);
+    }
+
+    public void Credits()
+    {
+        _creditsScreen.SetActive(true);
+        Debug.Log("This works");
+        _peopleList.GetComponent<Animator>().Play(0);
+        Debug.Log("This also works");
+
+        /*if (Input.GetButtonDown("Escape"))
+        {
+            _creditsScreen.SetActive(false);
+        }*/
     }
 
     public void Quit()
