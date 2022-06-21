c1cea8ed051f915ae95ebdf2fe2ca1ac2e663bb8
Adrien-M-design <61044812+Adrien-M-design@users.noreply.github.com>
2022-06-21 12:59:35 +0200

    sound update

--- Assets/Scripts/Engine/GameState.cs
+++ Assets/Scripts/Engine/GameState.cs
@@ -13,7 +13,7 @@ public class GameState : AGameState
     #region Methods
     public override void EnterState()
     {
-        BFMODManager.Instance.PlayHarborMusic();
+        //BFMODManager.Instance.PlayHarborMusic();
         BFMODManager.Instance.PlayDocksSound();
     }
 
